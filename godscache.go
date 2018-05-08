4b581d8697f6a1b83e48a4bbd46e924d506f06f8
Jeremy Carter <Jeremy@JeremyCarter.ca>
2018-05-08 18:33:16 -0400

    add test for godscache and make the struct private

--- godscache.go
+++ godscache.go
@@ -1,12 +1,33 @@
 package godscache
 
-import "log"
+import (
+	"context"
+	"log"
 
-type Godscache struct {
+	"cloud.google.com/go/datastore"
+)
+
+type godscache struct {
+	gcpProjectID string
+	ctx          context.Context
+	dsClient     *datastore.Client
 }
 
-func NewGodscache() *Godscache {
-	g := &Godscache{}
+func NewGodscache(gcpProjectID string) *godscache {
+	ctx := context.Background()
+
+	dsClient, err := datastore.NewClient(ctx, gcpProjectID)
+	if err != nil {
+		log.Printf("Error: Failed creating new datastore client: %v", err)
+		return nil
+	}
+
+	g := &godscache{
+		gcpProjectID: gcpProjectID,
+		ctx:          ctx,
+		dsClient:     dsClient,
+	}
+
 	log.Printf("Instantiated new Godscache: %+v", g)
 	return g
 }
--- godscache_test.go
+++ godscache_test.go
@@ -1,10 +1,36 @@
 package godscache
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
-func TestNewGodscache(t *testing.T) {
-	g := NewGodscache()
+// Things that should succeed.
+func TestNewGodscacheWithValidProjectID(t *testing.T) {
+	g := NewGodscache("godscache")
+
 	if g == nil {
-		t.Fatalf("Instantiating new Godscache struct failed")
+		t.Fatalf("Instantiating new Godscache struct with a valid GCP project ID failed")
+	}
+}
+
+func TestNewGodscacheWithProjectIDFromEnvVar(t *testing.T) {
+	os.Setenv("DATASTORE_PROJECT_ID", "godscache")
+
+	g := NewGodscache("")
+
+	os.Unsetenv("DATASTORE_PROJECT_ID")
+
+	if g == nil {
+		t.Fatalf("Instantiating new Godscache struct with project ID in the DATASTORE_PROJECT_ID environment variable failed")
+	}
+}
+
+// Things that should fail.
+func TestNewGodscacheWithNoProjectID(t *testing.T) {
+	g := NewGodscache("")
+
+	if g != nil {
+		t.Fatalf("Instantiating new Godscache struct with no project ID succeeded")
 	}
 }
